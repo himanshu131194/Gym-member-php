d2db449b0ad177c00a58f6603a2d3a9beda3a6d7
Himanshu Savita <himanshu131194@gmail.com>
2022-05-01 22:57:37 +0530

    form completed

--- planDetails.php
+++ planDetails.php
@@ -9,7 +9,7 @@ if($res){
 	// echo "<tr><td>".$row['amount']."</td></tr>";
 	echo "<tr>
 		<td height='35'>AMOUNT:</td>
-		<td height='35'>&nbsp;&ensp;&ensp;&ensp;&ensp;&ensp;&ensp;&ensp;&ensp;&ensp;&ensp;&ensp;&ensp;&ensp;&ensp;&ensp;&ensp;<input id='boxx' type='text' value='".$row['amount']."' readonly><br></td></tr>
+		<td height='35'>&nbsp;&ensp;&ensp;&ensp;&ensp;&ensp;&ensp;&ensp;&ensp;&ensp;&ensp;&ensp;&ensp;&ensp;&ensp;&ensp;&ensp;<input id='boxx' type='text' value='".$row['amount']."' readonly><br>git </td></tr>
 		<tr>
 		<td height='35'>VALIDITY:</td>
 		<td height='35'>&nbsp;&ensp;&ensp;&ensp;&ensp;&ensp;&ensp;&ensp;&ensp;&ensp;&ensp;&ensp;&ensp;&ensp;&ensp;&ensp;&ensp;<input type='text' id='boxx' value='".$row['validity']." Month' readonly></td>
